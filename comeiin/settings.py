f73996518e26f56d86562b9e4513b2342d29414e
Akani01 <akaniivinmiyen@gmail.com>
2026-09-19 03:16:21 +0200

    root

--- comeiin/settings.py
+++ comeiin/settings.py
@@ -72,7 +72,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
-
+USE_X_FORWARDED_HOST = True
 
 # ============================================================
 # SESSION SETTINGS
